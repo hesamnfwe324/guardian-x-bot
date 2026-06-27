b32b8380324e7a4eecca294d479c9e9d91fb494c
hesamnfwe324 <hasm95037@gmail.com>
2026-06-28 01:59:57 +0330

    Add bot/middlewares/__init__.py

---
+++ bot/middlewares/__init__.py
@@ -0,0 +1,12 @@
+from bot.middlewares.i18n import I18nMiddleware, load_translations, get_text
+from bot.middlewares.throttling import ThrottlingMiddleware, FloodControlMiddleware
+from bot.middlewares.database import DatabaseMiddleware
+
+__all__ = [
+    "I18nMiddleware",
+    "load_translations",
+    "get_text",
+    "ThrottlingMiddleware",
+    "FloodControlMiddleware",
+    "DatabaseMiddleware",
+]
