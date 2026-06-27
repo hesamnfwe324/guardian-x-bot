b3e4c33292248666b90b0d0bc65bf938eebf3b75
hesamnfwe324 <hasm95037@gmail.com>
2026-06-28 01:59:45 +0330

    Add bot/keyboards/__init__.py

---
+++ bot/keyboards/__init__.py
@@ -0,0 +1,13 @@
+from bot.keyboards.main_menu import language_selection_kb, main_menu_kb, back_button_kb, confirm_cancel_kb
+from bot.keyboards.security import security_menu_kb, locks_menu_kb, captcha_menu_kb
+from bot.keyboards.games import games_menu_kb, dice_menu_kb
+from bot.keyboards.economy import economy_menu_kb, wallet_kb
+from bot.keyboards.moderation import moderation_menu_kb
+
+__all__ = [
+    "language_selection_kb", "main_menu_kb", "back_button_kb", "confirm_cancel_kb",
+    "security_menu_kb", "locks_menu_kb", "captcha_menu_kb",
+    "games_menu_kb", "dice_menu_kb",
+    "economy_menu_kb", "wallet_kb",
+    "moderation_menu_kb",
+]
