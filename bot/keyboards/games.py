7c6574960a681a337ca2630d97c793fd57b64555
hesamnfwe324 <hasm95037@gmail.com>
2026-06-28 23:28:32 +0330

    fix: add all missing keyboard stubs (game_action_kb, join_game_kb, mines_board_kb, ban_duration_kb, notes_kb)

--- bot/keyboards/games.py
+++ bot/keyboards/games.py
@@ -1,29 +1,12 @@
 from typing import Callable
 
-
-def games_menu_kb(_):
-    return None
-
-
-def dice_menu_kb(_):
-    return None
-
-
-def rps_choice_kb(_):
-    return None
-
-
-def wheel_spin_kb(_):
-    return None
-
-
-def game_result_kb(_):
-    return None
-
-
-def tournament_kb(_):
-    return None
-
-
-def duel_kb(_, duel_id=0):
-    return None
+def games_menu_kb(_): return None
+def dice_menu_kb(_): return None
+def game_action_kb(_, **kw): return None
+def join_game_kb(_, game_id=0): return None
+def rps_choice_kb(_): return None
+def wheel_spin_kb(_): return None
+def mines_board_kb(_, **kw): return None
+def game_result_kb(_): return None
+def tournament_kb(_): return None
+def duel_kb(_, duel_id=0): return None
--- bot/keyboards/moderation.py
+++ bot/keyboards/moderation.py
@@ -1,21 +1,9 @@
 from typing import Callable
 
-
-def moderation_menu_kb(_, is_group=False):
-    return None
-
-
-def mute_duration_kb(_, user_id=0):
-    return None
-
-
-def warn_actions_kb(_, user_id=0):
-    return None
-
-
-def warn_kb(_, user_id=0):
-    return None
-
-
-def ban_confirm_kb(_, user_id=0):
-    return None
+def moderation_menu_kb(_, is_group=False): return None
+def mute_duration_kb(_, user_id=0): return None
+def ban_duration_kb(_, user_id=0): return None
+def warn_actions_kb(_, user_id=0): return None
+def notes_kb(_, **kw): return None
+def warn_kb(_, user_id=0): return None
+def ban_confirm_kb(_, user_id=0): return None
