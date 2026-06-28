8d4033591e003fc98dc59d00e6d6ede687605a64
hesamnfwe324 <hasm95037@gmail.com>
2026-06-28 13:43:20 +0330

    fix: remove confirm_cancel_kb import (function did not exist in main_menu.py)

--- bot/keyboards/__init__.py
+++ bot/keyboards/__init__.py
@@ -1,11 +1,11 @@
-from bot.keyboards.main_menu import language_selection_kb, main_menu_kb, back_button_kb, confirm_cancel_kb
+from bot.keyboards.main_menu import language_selection_kb, main_menu_kb, back_button_kb
 from bot.keyboards.security import security_menu_kb, locks_menu_kb, captcha_menu_kb
 from bot.keyboards.games import games_menu_kb, dice_menu_kb
 from bot.keyboards.economy import economy_menu_kb, wallet_kb
 from bot.keyboards.moderation import moderation_menu_kb
 
 __all__ = [
-    "language_selection_kb", "main_menu_kb", "back_button_kb", "confirm_cancel_kb",
+    "language_selection_kb", "main_menu_kb", "back_button_kb",
     "security_menu_kb", "locks_menu_kb", "captcha_menu_kb",
     "games_menu_kb", "dice_menu_kb",
     "economy_menu_kb", "wallet_kb",
