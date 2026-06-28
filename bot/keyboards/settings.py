46fa8dca3a07e3d2f56d3e4ba7299453891f7284
hesamnfwe324 <hasm95037@gmail.com>
2026-06-28 23:10:31 +0330

    refactor: remove settings inline keyboards

--- bot/keyboards/settings.py
+++ bot/keyboards/settings.py
@@ -1,18 +1,14 @@
-from aiogram.types import InlineKeyboardMarkup
-from aiogram.utils.keyboard import InlineKeyboardBuilder
 from typing import Callable
 
 
-def settings_menu_kb(_: Callable) -> InlineKeyboardMarkup:
-    """Beautiful i18n settings menu."""
-    builder = InlineKeyboardBuilder()
-    builder.button(text=_('btn_welcome'),       callback_data="settings:welcome")
-    builder.button(text=_('btn_goodbye'),       callback_data="settings:goodbye")
-    builder.button(text=_('btn_log_channel'),   callback_data="settings:logs")
-    builder.button(text=_('btn_general_settings'), callback_data="settings:general")
-    builder.button(text=_('btn_slow_mode'),     callback_data="settings:slowmode")
-    builder.button(text=_('btn_auto_delete'),   callback_data="settings:autodel")
-    builder.button(text=_('btn_back'),          callback_data="menu:main")
-    builder.button(text=_('btn_home'),          callback_data="menu:main")
-    builder.adjust(2, 2, 2, 2)
-    return builder.as_markup()
+  def settings_menu_kb(_: Callable):
+      return None
+
+
+  def welcome_settings_kb(_: Callable, settings=None):
+      return None
+
+
+  def log_channel_kb(_: Callable):
+      return None
+  
