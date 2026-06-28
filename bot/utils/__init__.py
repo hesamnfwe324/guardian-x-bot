2383a310c8bd9836d3b07604c30cf98c8c168b5c
hesamnfwe324 <hasm95037@gmail.com>
2026-06-28 13:30:03 +0330

    fix: remove 2-space IndentationError from utils/__init__.py

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -1,13 +1,13 @@
 from bot.utils.helpers import (
-    format_duration, parse_duration, format_number,
-    xp_for_level, calculate_level, progress_bar,
-    mention_user, get_time_until, truncate, safe_username
+  format_duration, parse_duration, format_number,
+  xp_for_level, calculate_level, progress_bar,
+  mention_user, get_time_until, truncate, safe_username
 )
 from bot.utils.logger import setup_logging
 
 __all__ = [
-    "format_duration", "parse_duration", "format_number",
-    "xp_for_level", "calculate_level", "progress_bar",
-    "mention_user", "get_time_until", "truncate", "safe_username",
-    "setup_logging",
+  "format_duration", "parse_duration", "format_number",
+  "xp_for_level", "calculate_level", "progress_bar",
+  "mention_user", "get_time_until", "truncate", "safe_username",
+  "setup_logging",
 ]
