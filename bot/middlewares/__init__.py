452882b322f4d06f3e3c48d7508950cc8470ef9c
hesamnfwe324 <hasm95037@gmail.com>
2026-06-28 13:27:29 +0330

    fix: remove 2-space IndentationError from middlewares/__init__.py

--- bot/middlewares/__init__.py
+++ bot/middlewares/__init__.py
@@ -1,15 +1,14 @@
 from bot.middlewares.i18n import I18nMiddleware, load_translations, get_text
-  from bot.middlewares.throttling import ThrottlingMiddleware, FloodControlMiddleware
-  from bot.middlewares.database import DatabaseMiddleware
-  from bot.middlewares.callback_logging import CallbackLoggingMiddleware
+from bot.middlewares.throttling import ThrottlingMiddleware, FloodControlMiddleware
+from bot.middlewares.database import DatabaseMiddleware
+from bot.middlewares.callback_logging import CallbackLoggingMiddleware
 
-  __all__ = [
-      "I18nMiddleware",
-      "load_translations",
-      "get_text",
-      "ThrottlingMiddleware",
-      "FloodControlMiddleware",
-      "DatabaseMiddleware",
-      "CallbackLoggingMiddleware",
-  ]
-  
+__all__ = [
+    "I18nMiddleware",
+    "load_translations",
+    "get_text",
+    "ThrottlingMiddleware",
+    "FloodControlMiddleware",
+    "DatabaseMiddleware",
+    "CallbackLoggingMiddleware",
+]
